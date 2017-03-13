5bdfb5a7c6d5c1bdf8a092fee61570892b378455
Alexander Davidson <alexander.harlan.davidson@gmail.com>
2017-03-13 11:11:26 -0700

    push to examine text centering problem

--- typing-test/main.js
+++ typing-test/main.js
@@ -12,9 +12,18 @@ var blank = []
 // function to create single letters into individual <span>'s
 //Issue - its creating a span of the whole array of individual letters
 
-for (i = 0; i < textArrayLength; i++) {
-  blank.push('<span>' + textArray[i] + '</span>')
+var $divCenter = document.createElement('div')
+
+for (var i = 0; i < textArrayLength; i++) {
+  var $text = document.createElement('span')
+  $text.className = 'center'
+  $text.textContent = text[i]
+  document.getElementById('testText').appendChild($text)
 }
+// need to nest 'center' into div to be able to center
+
+document.addEventListener('DOMContentLoaded')
+
 
 /* g
 function sentence(characters) {
